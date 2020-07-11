3869de8122755d3a3ac2742099cb6b37790c6086
Andrew Van Horn <andrewvh7@gmail.com>
2020-07-11 17:48:05 -0500

    created fucntion to retrieve and store data

--- OpenWeather.py
+++ OpenWeather.py
@@ -3,15 +3,6 @@ import json
 #import numpy as np
 from datetime import *
 
-APIKey = "NULL"
-
-def getAPIKey():
-	global APIKey
-	with open("API_KEY.txt", 'r') as f:
-		lines = f.readlines()
-		APIKey = lines[0].replace('\n', '')
-		print(APIKey)
-	return(APIKey)
 
 def getLocation(location):
 	with open("Locations.txt", 'r') as f:
@@ -23,24 +14,59 @@ def getLocation(location):
 		print("Error, Location not found:" + location)
 		return(0,0)
 
-def OneCall(lat, lon):
-	global APIKey
-	r = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid="+APIKey)
-	print(r.text)
-	data_JSON = json.loads(r.text)
-	results = []
-	'''
-	if(data_JSON!={}):
-		results = [item for item in data_JSON['results'] if item['datatype']=='TAVG']
-	data = []
-	for item in results:
-		date = datetime.strptime(item['date'], "%Y-%m-%dT%H:%M:%S")
-		value = float(item['value'])
-		data = data + [[date, value]]
+class OpenWeather():
+	APIKey = "NULL"
+	
+	def init():
+		OpenWeather.getAPIKey()
+		try:
+			open("CallLogs.json", 'r')
+		except:
+			OpenWeather.makeDataLog()
+		
+	def getAPIKey():
+		global APIKey
+		with open("API_KEY.txt", 'r') as f:
+			lines = f.readlines()
+			APIKey = lines[0].replace('\n', '')
+			#print(APIKey)
+		return(APIKey)
+	def makeDataLog():
+		print("Creating New Datalog File")
+		with open("CallLogs.json", 'w+') as f:
+			data = {}
+			data['Entries']=[]
+			json.dump(data, f)
+			
+	def OneCall(lat, lon):	
+		r = requests.get("https://api.openweathermap.org/data/2.5/onecall?lat="+str(lat)+"&lon="+str(lon)+"&appid="+APIKey)
+		return(json.loads(r.text))
+		
+	def storeData(lat, lon, time, data):
+		
+		with open("CallLogs.json", 'r') as dataLogFile:
+			dataLog = json.load(dataLogFile)
+	
+		dataLog['Entries'].append(data)
+			
+		with open("CallLogs.json", 'w') as dataLogFile:
+			json.dump(dataLog, dataLogFile)
+		
+		
 		'''
-	#return(data)
+		if(data_JSON!={}):
+			results = [item for item in data_JSON['results'] if item['datatype']=='TAVG']
+		data = []
+		for item in results:
+			date = datetime.strptime(item['date'], "%Y-%m-%dT%H:%M:%S")
+			value = float(item['value'])
+			data = data + [[date, value]]
+			'''
 	
+		
 	
-getAPIKey()
-print(getLocation("StL"))
-print(APIKey)
+
+OpenWeather.init()
+
+lat, lon = getLocation("StL")
+OpenWeather.storeData(lat, lon, str(datetime.now()), OpenWeather.OneCall(lat, lon))
